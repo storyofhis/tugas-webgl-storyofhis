de08b56e8ef1e34468a0433d4575839d3197f63a
Maula Izza Azizi <azizi.maula@gmail.com>
2022-09-21 03:52:34 +0700

    feat: finish all

--- src/main.js
+++ src/main.js
@@ -188,6 +188,7 @@ const main = () => {
   gl.shaderSource(vertexShaderObject, vertexShaderCode);
   gl.compileShader(vertexShaderObject);
 
+  // ganti warna tulisan
   let fragmentShaderCode = `
   precision mediump float;
   void main() {
@@ -212,12 +213,12 @@ const main = () => {
   gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(aPosition);
 
-  gl.clearColor(1.0, 0.65, 0.0, 1.0);
+  // gl.clearColor(1.0, 0.65, 0.0, 1.0);
+  
+  gl.clearColor(0.0, 0.0, 0.0, 0.0);
+  // initBkgnd();
+  gl.enable(gl.DEPTH_TEST);
   gl.clear(gl.COLOR_BUFFER_BIT);
-  //   gl.drawArrays(gl.LINE_LINES, 0, 4);
-  gl.lineWidth(17);
-  // gl.drawArrays(gl.TRIANGLE_STRIP, 0, 13);
-  // gl.drawArrays(gl.TRIANGLE_STRIP, 13, 11);
 
   // // I
   gl.drawArrays(gl.TRIANGLES, 0, 3);
@@ -232,74 +233,20 @@ const main = () => {
   gl.drawArrays(gl.LINE_LOOP, 145, 12)
 };
 
+// function initBkgnd(){
+//   backTex = gl.createTexture();
+//   backTex.Img = new Image();
+//   backTex.Img.onload = function() {
+//     handleBkTex(backTex);
+//   }
+//   backTex.Img.src = "./assets/macbook.svg"
+// }
 
-const hurufI = () => {
-  const canvasI = document.getElementById("canvasi")
-  const gl = canvasI.getContext("webgl")
-
-  let vertices = [
-    0.13671, 0.79998, -0.15527, 0.80539, -0.17238, -0.79106,
-
-    0.1679, 0.79998, 0.16058, -0.79106, -0.15958, -0.79055,
-  ];
-
-  let buffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-  let vertexShaderCode = `
-    attribute vec2 aPosition;
-    void main() {
-        float x = aPosition.x;
-        float y = aPosition.y;
-        gl_PointSize = 5.0;
-        gl_Position = vec4(x, y, 0.0, 1.0);
-    }
-  `;
-  let vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vertexShaderObject, vertexShaderCode);
-  gl.compileShader(vertexShaderObject);
-
-  let fragmentShaderCode = `
-    precision mediump float;
-    // varying vec3 fragColor;
-    void main() {
-        float r = 0.0;
-        float g = 0.0;
-        float b = 1.0;
-        gl_FragColor = vec4(r, g, b, 1.0);
-    }
-  `;
-
-  let fragmentShaderCode2 = `
-      precision mediump float;
-      void main() {
-          float r = 0.0;
-          float g = 1.0;
-          float b = 0.0;
-          gl_FragColor = vec4(r, g, b, 1.0);
-      }
-    `;
-
-    let fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
-    gl.shaderSource(fragmentShaderObject, fragmentShaderCode2); //
-    gl.compileShader(fragmentShaderObject);
-
-    let program = gl.createProgram();
-  gl.attachShader(program, vertexShaderObject);
-  gl.attachShader(program, fragmentShaderObject);
-  //   gl.attachShader(program, fragmentShaderObject2);
-  gl.linkProgram(program);
-  gl.useProgram(program);
-
-  let aPosition = gl.getAttribLocation(program, "aPosition");
-  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(aPosition);
-
-  gl.clearColor(1.0, 0.65, 0.5, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
-  gl.drawArrays(gl.TRIANGLES, 3, 3);
-}
+// function handleBkTex(tex) {
+//   gl.bindTexture(gl.TEXTURE_2D, tex);
+//   gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+//   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, tex.Img);
+//   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+//   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+//   gl.bindTexture(gl.TEXTURE_2D, null);
+// }
